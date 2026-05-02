b7a3375a1ef573f377ee61afd509fa78dba0eae5
OmGadge01 <omgadge19434@gmail.com>
2026-05-02 16:13:02 +0530

    backend url added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://tasktracker-z39a.onrender.com";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL,
 });
 
 export default API;
+
